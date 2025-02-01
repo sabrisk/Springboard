f02aa87e8ae8c88a5ee72799582e8dc78d105eee
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-01-31 23:51:23 -0800

    Modifies code to add up vacation days earned.

--- Exercises/8.1.9-Error_Handling/detective.js
+++ Exercises/8.1.9-Error_Handling/detective.js
@@ -1,13 +1,31 @@
-function mysteryOperation ()
-{
+function mysteryOperation() {
+
 	const outcome = Math.random(); // Generates a random number between 0 and 1.
 
-	if (outcome < 0.5)
-	{
+	if (outcome < 0.5) {
 		console.log("The operation is completed successfully!");
 	}
-	else
-	{
+	else {
 		throw new Error("The operation is failed mysteriously!");
 	}
 }
+
+
+const OPERATIONS_ATTENDED = 20;
+const VACATION_DAYS_FOR_SUCCESS = 13;
+const VACATION_DAYS_FOR_FAILURE = 1;
+const DAY_REWARD_PER_ATTENDANCE = 3;
+
+let vacationDays = 0;
+
+for (let i = 0; i < OPERATIONS_ATTENDED; i++) {
+	try {
+		mysteryOperation();
+		vacationDays += VACATION_DAYS_FOR_SUCCESS;
+	} catch (err) {
+		vacationDays += VACATION_DAYS_FOR_FAILURE;
+	} finally {
+		vacationDays += DAY_REWARD_PER_ATTENDANCE;
+	}
+}
+console.log("Vacation days earned: ", vacationDays)
