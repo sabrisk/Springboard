593cef364ace1bd3ae46c9de4e59e766d587869b
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-01-31 22:31:51 -0800

    Initial commit.

---
+++ Exercises/8.1.9-Error_Handling/detective.js
@@ -0,0 +1,13 @@
+function mysteryOperation ()
+{
+	const outcome = Math.random(); // Generates a random number between 0 and 1.
+
+	if (outcome < 0.5)
+	{
+		console.log("The operation is completed successfully!");
+	}
+	else
+	{
+		throw new Error("The operation is failed mysteriously!");
+	}
+}
