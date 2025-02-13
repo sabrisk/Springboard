ab9c66a27005d2f237f718d2bd986806db052006
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-02-13 08:53:50 -0800

    Adds the rest of the sorting algorithms.

--- Exercises/8.3.14-Sorting/quick.js
+++ Exercises/8.3.14-Sorting/quick.js
@@ -1,24 +1,31 @@
-/*
-pivot accepts an array, starting index, and ending index
-You can assume the pivot is always the first element
-*/
+function pivot(arr, start = 0, end = arr.length - 1) {
+	const swap = (arr, idx1, idx2) => {
+		[arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+	};
 
-function pivot(arr, startIdx, endIdx) {
-	let pivotIdx = 0;
-	let countLeft = 0;
+	// We are assuming the pivot is always the first element
+	let pivot = arr[start];
+	let swapIdx = start;
 
-	for (let i = 1; i < arr.length; i++) {
-		if (arr[i] < arr[pivotIdx]) {
-			countLeft++;
+	for (let i = start + 1; i <= end; i++) {
+		if (pivot > arr[i]) {
+			swapIdx++;
+			swap(arr, swapIdx, i);
 		}
 	}
-	return countLeft;
+
+	// Swap the pivot from the start the swapPoint
+	swap(arr, start, swapIdx);
+	return swapIdx;
 }
 
-/*
-quickSort accepts an array, left index, and right index
-*/
+function quickSort(arr, left = 0, right = arr.length - 1) {
+	if (left < right) {
+		let pivotIndex = pivot(arr, left, right);
+		quickSort(arr, left, pivotIndex - 1);
+		quickSort(arr, pivotIndex + 1, right);
+	}
+	return arr;
+}
 
-function quickSort() {}
-
-module.exports = quickSort;
+module.exports = { pivot, quickSort };
