0aca556630579399e60e3c805392040c641e360a
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-02-15 20:18:35 -0800

    Adds try catch statements.

--- Exercises/8.5.8-Promises/NumberFactsPromises.js
+++ Exercises/8.5.8-Promises/NumberFactsPromises.js
@@ -2,33 +2,42 @@ const numbersAPI = "http://numbersapi.com/";
 
 async function getNumberFact() {
 	const num = 54;
-	const response = await fetch(numbersAPI + num + "?json");
-	const json = await response.json();
-	const titleElem = document.createElement("p");
+	try {
+		const response = await fetch(numbersAPI + num + "?json");
+		const json = await response.json();
 
-	titleElem.textContent = "-----Get a single number fact-----";
-	document.body.append(titleElem);
+		const titleElem = document.createElement("p");
 
-	const p = document.createElement("p");
-	p.textContent = json.text;
-	document.body.append(p);
+		titleElem.textContent = "-----Get a single number fact-----";
+		document.body.append(titleElem);
+
+		const p = document.createElement("p");
+		p.textContent = json.text;
+		document.body.append(p);
+	} catch (err) {
+		console.error("Error fetching data for getNumberFact():", err);
+	}
 }
 
 async function getNumberFacts() {
 	const numRanges = "1..10";
-	const response = await fetch(numbersAPI + numRanges);
-	const json = await response.json();
-	const titleElem = document.createElement("p");
+	try {
+		const response = await fetch(numbersAPI + numRanges);
+		const json = await response.json();
+		const titleElem = document.createElement("p");
 
-	titleElem.textContent = "-----Get multiple number facts-----";
-	document.body.append(titleElem);
+		titleElem.textContent = "-----Get multiple number facts-----";
+		document.body.append(titleElem);
 
-	for (const key in json) {
-		const p = document.createElement("p");
-		if (json.hasOwnProperty(key)) {
-			p.textContent = json[key];
-			document.body.append(p);
+		for (const key in json) {
+			const p = document.createElement("p");
+			if (json.hasOwnProperty(key)) {
+				p.textContent = json[key];
+				document.body.append(p);
+			}
 		}
+	} catch (err) {
+		console.error("Error fetching data for getNumberFacts():", err);
 	}
 }
 
@@ -36,23 +45,27 @@ async function getFavoriteNumberFacts() {
 	const num = 7;
 	const numFacts = 7;
 	const promisesArr = [];
-	for (let i = 0; i < numFacts; i++) {
-		const numberPromise = fetch(numbersAPI + num + "?json").then((res) =>
-			res.json()
-		);
-		promisesArr.push(numberPromise);
-	}
-	const responses = await Promise.all(promisesArr);
+	try {
+		for (let i = 0; i < numFacts; i++) {
+			const numberPromise = fetch(numbersAPI + num + "?json").then(
+				(res) => res.json()
+			);
+			promisesArr.push(numberPromise);
+		}
+		const responses = await Promise.all(promisesArr);
 
-	const titleElem = document.createElement("p");
+		const titleElem = document.createElement("p");
 
-	titleElem.textContent =
-		"------Get " + numFacts + " favorite number facts-----";
-	document.body.append(titleElem);
+		titleElem.textContent =
+			"------Get " + numFacts + " favorite number facts-----";
+		document.body.append(titleElem);
 
-	for (r of responses) {
-		const p = document.createElement("p");
-		p.textContent = r.text;
-		document.body.append(p);
+		for (r of responses) {
+			const p = document.createElement("p");
+			p.textContent = r.text;
+			document.body.append(p);
+		}
+	} catch (err) {
+		console.error("Error fetching data for getFavoriteNumberFacts():", err);
 	}
 }
