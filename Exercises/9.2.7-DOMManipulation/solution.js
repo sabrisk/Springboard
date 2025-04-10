2409eca9ec4ccb24053d1602129fab6848b31cb6
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-04-09 17:00:15 -0700

    DOM Manipulation Exercise

---
+++ Exercises/9.2.7-DOMManipulation/solution.js
@@ -0,0 +1,21 @@
+document.getElementById("task1").innerText = "Changed using 'innerText'.";
+document.getElementById("task2").innerHTML =
+	'<button type="button">Submit</button>';
+document.body.style.backgroundColor = "#232323";
+const itemElements = document.getElementsByClassName("item");
+for (let i of itemElements) {
+	i.style.border = "2px solid black";
+}
+document
+	.getElementById("task5")
+	.setAttribute("href", "https://www.springboard.com/");
+document.getElementById("task6").value = "DOM Master";
+document.getElementById("task7").classList.add("new-class");
+const task8Div = document.getElementById("task8");
+
+const button = document.createElement("button");
+button.textContent = "A button";
+button.type = "button"; // Set attributes
+
+task8Div.append(button);
+document.getElementById("task9").remove();
