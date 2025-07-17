f9f1c8ee87ab31fd346689699f26ca3b962f188f
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-07-17 01:54:06 -0700

    Builds out footer component.

--- Assessments/space-travel/src/components/Footer/Footer.jsx
+++ Assessments/space-travel/src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-	return <footer className={styles.footer}></footer>;
+	return (
+		<footer className={styles.footer}>
+			<span>The solar system: the new home.</span>
+			<span>🌍🚀👩‍🚀🪐</span>
+		</footer>
+	);
 };
 
 export default Footer;
