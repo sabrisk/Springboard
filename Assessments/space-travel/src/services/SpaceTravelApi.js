3b9fa9727aefd7ce6da9684804a3c9450e69b927
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-07-20 00:11:44 -0700

    Api file saved with prettier caused formatting changes.

--- Assessments/space-travel/src/services/SpaceTravelApi.js
+++ Assessments/space-travel/src/services/SpaceTravelApi.js
@@ -1,36 +1,42 @@
 import SpaceTravelMockApi from "./SpaceTravelMockApi.js";
 
-class SpaceTravelApi
-{
-  static async getPlanets ()
-  {
-    return SpaceTravelMockApi.getPlanets();
-  }
+class SpaceTravelApi {
+	static async getPlanets() {
+		return SpaceTravelMockApi.getPlanets();
+	}
 
-  static async getSpacecrafts ()
-  {
-    return SpaceTravelMockApi.getSpacecrafts();
-  }
+	static async getSpacecrafts() {
+		return SpaceTravelMockApi.getSpacecrafts();
+	}
 
-  static async getSpacecraftById ({id})
-  {
-    return SpaceTravelMockApi.getSpacecraftById({id});
-  }
+	static async getSpacecraftById({ id }) {
+		return SpaceTravelMockApi.getSpacecraftById({ id });
+	}
 
-  static async buildSpacecraft ({name, capacity, description, pictureUrl = undefined})
-  {
-    return SpaceTravelMockApi.buildSpacecraft({name, capacity, description, pictureUrl});
-  }
+	static async buildSpacecraft({
+		name,
+		capacity,
+		description,
+		pictureUrl = undefined,
+	}) {
+		return SpaceTravelMockApi.buildSpacecraft({
+			name,
+			capacity,
+			description,
+			pictureUrl,
+		});
+	}
 
-  static async destroySpacecraftById ({id})
-  {
-    return SpaceTravelMockApi.destroySpacecraftById({id});
-  }
+	static async destroySpacecraftById({ id }) {
+		return SpaceTravelMockApi.destroySpacecraftById({ id });
+	}
 
-  static async sendSpacecraftToPlanet ({spacecraftId, targetPlanetId})
-  {
-    return SpaceTravelMockApi.sendSpacecraftToPlanet({spacecraftId, targetPlanetId});
-  }
+	static async sendSpacecraftToPlanet({ spacecraftId, targetPlanetId }) {
+		return SpaceTravelMockApi.sendSpacecraftToPlanet({
+			spacecraftId,
+			targetPlanetId,
+		});
+	}
 }
 
 export default SpaceTravelApi;
