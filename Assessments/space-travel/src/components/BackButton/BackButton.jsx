8292fcac2cba761cb9977e6d83800835b78168a1
sabrisk <4885090+sabrisk@users.noreply.github.com>
2025-07-23 00:56:37 -0700

    Fixes incorrect className prop.

--- Assessments/space-travel/src/components/BackButton/BackButton.jsx
+++ Assessments/space-travel/src/components/BackButton/BackButton.jsx
@@ -1,26 +1,12 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./BackButton.module.css";
 
-// const BackButton = ({ path, emoji, name }) => {
-// 	return (
-// 		// <NavLink
-// 		// 	className={({ isActive }) =>
-// 		// 		isActive ? `${styles.link} ${styles.active}` : `${styles.link}`
-// 		// 	}
-// 		// 	to={path}
-// 		// >
-// 		// 	{emoji} {name}
-// 		// </NavLink>
-// 		<button></button>
-// 	);
-// };
-
 function BackButton() {
 	const navigate = useNavigate();
 
 	return (
 		<button onClick={() => navigate(-1)} className={styles.button}>
-			Go Back <span class={styles.emoji}>👈</span>
+			Go Back <span className={styles.emoji}>👈</span>
 		</button>
 	);
 }
